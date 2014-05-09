beedaa96edea2ce1d633594404f1921b023cd96c
Petr Kočí <petr.koci@gmail.com>
2014-05-09 16:53:01 +0200

    základní histogram

--- clean.R
+++ clean.R
@@ -1,4 +1,7 @@
-vysledky  <- data.frame(cas=as.difftime(cas, "%X", units="secs"),
+library(stringr)
+library(jsonlite)
+
+vysledky <- data.frame(cas=as.difftime(cas, "%X", units="secs"),
                         cas_cip=as.difftime(cas_cip, "%X", units="secs"),
                         cislo,
                         jmeno,
@@ -9,3 +12,20 @@ vysledky  <- data.frame(cas=as.difftime(cas, "%X", units="secs"),
                         umisteni_celkove=as.numeric(umisteni_celkove),
                         umisteni_kategorie=as.numeric(umisteni_kategorie),
                         umisteni_pohlavi=as.numeric(umisteni_pohlavi))
+
+vysledky$jmeno <- sapply(str_split(vysledky$jmeno, "Číslo: "), function (x) {return(x[1])})
+
+
+vysledky$cislo <- as.numeric(str_sub(vysledky$cislo, 8))
+
+vysledky$kategorie  <- factor(str_trim(vysledky$kategorie))
+
+export  <- data.frame(vysledky$cas, vysledky$kategorie, vysledky$narodnost)
+
+export  <- export[export$vysledky.cas>0, ]
+
+export$vysledky.cas  <- as.numeric(export$vysledky.cas)
+
+names(export)  <- c("c", "k", "n")
+
+writeLines(toJSON(export), "vysledky2013.json")
